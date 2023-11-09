81c2292e6460632402ba5dfefa864a49a8ccecd6
Pavel Smiga <smigapav@fel.cvut.cz>
2023-11-09 22:00:34 +0100

    Changed Sprint

--- src/main/kotlin/cz/cvut/fel/ear/ear_project/model/Sprint.kt
+++ src/main/kotlin/cz/cvut/fel/ear/ear_project/model/Sprint.kt
@@ -1,31 +1,30 @@
-//package cz.cvut.fel.ear.ear_project.model
-//
-//import jakarta.persistence.*
-//
-//@Entity
+package cz.cvut.fel.ear.ear_project.model
+
+import jakarta.persistence.*
+
 //@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 //@DiscriminatorColumn(name = "SPRINT_TYPE")
-//@Table(name = "Sprints")
-//abstract class Sprint : AbstractEntity() {
-//
-//    @Column
-//    open var name: String? = null
-//
-//    @ManyToOne
-//    open var project: Project? = null
-//
-//    @OneToMany(mappedBy = "sprint")
-//    open var stories: MutableList<Story>? = null
-//
-//    fun addStory(story: Story) {
-//        if(stories == null) {
-//            stories = mutableListOf()
-//        }
-//        stories?.add(story)
-//    }
-//
-//    fun removeStory(story: Story) {
-//        stories?.remove(story)
-//    }
-//}
-//
+@Entity
+@Table(name = "sprints")
+class Sprint : AbstractEntity() {
+
+    var name: String? = null
+
+    @ManyToOne
+    var project: Project? = null
+
+    @OneToMany(mappedBy = "sprint")
+    var stories: MutableList<Story> = mutableListOf()
+
+    fun addStory(story: Story) {
+        if(stories == null) {
+            stories = mutableListOf()
+        }
+        stories?.add(story)
+    }
+
+    fun removeStory(story: Story) {
+        stories?.remove(story)
+    }
+}
+
